4d4222e28ff2ba41755bfe8b04ea89856202846d
Bakaudin Elazcona <wotaboge@gmail.com>
2024-10-08 07:45:35 +0700

    add colorscheme

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd([[
 	set tabstop=2 softtabstop=2 shiftwidth=2
 	set hidden noswapfile nobackup
 	set fillchars=eob:\ 
+	set nowrap
 
 	set background=dark
 	colorscheme sorbet
@@ -10,21 +11,30 @@ vim.cmd([[
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-require("lazy").setup({ })
+require("lazy").setup(
+	{
+		"Tsuzat/NeoSolarized.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd [[ colorscheme NeoSolarized ]]
+		end
+	}
+)
