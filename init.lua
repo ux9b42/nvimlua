fc92168d9c904c5cd87d6c84b7659940960ef7dd
Bakaudin Elazcona <wotaboge@gmail.com>
2024-10-08 07:33:16 +0700

    simple config

---
+++ init.lua
@@ -0,0 +1,9 @@
+vim.cmd([[
+	set noexpandtab number
+	set tabstop=2 softtabstop=2 shiftwidth=2
+	set hidden noswapfile nobackup
+	set fillchars=eob:\ 
+
+	set background=dark
+	colorscheme sorbet
+]])
